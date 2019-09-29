1bde3bd70d0e2f0f29249d3b4cc1037bac4c87f9
nishkaush <kaushish.nishant@hotmail.com>
2019-09-29 10:28:41 +1000

    initial setup of the project, including a basic form with 3 inputs and the respective handlechange and handlesubmit method placeholders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formFieldsArr: [
+        {
+          id: 1,
+          name: "postcode",
+          value: "",
+          placeholder: "Postcode",
+          type: "number"
+        },
+        {
+          id: 2,
+          name: "suburb",
+          value: "",
+          placeholder: "Suburb",
+          type: "text"
+        },
+        {
+          id: 3,
+          name: "state",
+          value: "",
+          placeholder: "State",
+          type: "text"
+        }
+      ]
+    };
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(id, event) {
+    let foundFieldObj = { ...this.state.formFieldsArr.find(e => e.id === id) };
+    let foundFieldInd = id - 1;
+    foundFieldObj.value = event.target.value;
+    this.setState(state => {
+      state.formFieldsArr[foundFieldInd] = foundFieldObj;
+      return state;
+    });
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    // let payload = this.state.formFieldsArr.
+    console.log(this.state.formFieldsArr);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello LawPath</h1>
+        <form
+          className="lawpath__form"
+          action=""
+          onSubmit={this.handleFormSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {this.state.formFieldsArr.map(({ id, ...props }) => (
+            <input
+              key={id}
+              {...props}
+              onChange={this.handleInputChange.bind(this, id)}
+            />
+          ))}
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
